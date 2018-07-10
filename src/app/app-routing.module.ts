3fc2eb46597c83a65a4a1aebcfbc72ae3c8c6a14
Pradeep Kumar Gupta <pradeepk0141@gmail.com>
2018-07-11 04:56:31 +0530

    Routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HeroesComponent }      from './heroes/heroes.component';
+
+@NgModule({
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+  
+})
+
+export class AppRoutingModule { }
